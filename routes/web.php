90f8b90ae40003b4da68884b6318e5f4e13dd7f7
hiroya.mizobe <hiroya.mizobe@rakuten.com>
2018-05-29 04:18:55 +0000

    message model

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+
+Route::resource('messages', 'MessagesController');
+
+?>
+
+
+
