5c741b44cff877e0d66b59fde5c2c16780d96d72
hiroya.mizobe <hiroya.mizobe@rakuten.com>
2018-06-01 05:11:02 +0000

    validation

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -3,6 +3,14 @@
 @section('content')
 
     <h1>メッセージ新規作成ページ</h1>
+    
+    
+    
+    
+    
+    
+    
+    
 
     {!! Form::model($message, ['route' => 'messages.store']) !!}
 
@@ -16,7 +24,5 @@
 <!-- Write content for each page here -->
 
 
-    {{ '<p style="color: red;">When passed through htmlentities function</p>' }}
-    {!! '<p style="color: red;">When not passed through htmlentities function</p>' !!}
 
 @endsection
--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -14,3 +14,5 @@
     {!! Form::close() !!}
 
 @endsection
+
+
